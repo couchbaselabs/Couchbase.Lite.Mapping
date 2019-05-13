0ceb32799af06d16d4a4548f14e38f777b7041f1
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2019-05-12 19:17:18 -0500

    Added support for List<Result> to List<Object> conversion

---
+++ src/Couchbase.Lite.Mapping/ResultSetExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Couchbase.Lite.Mapping;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Couchbase.Lite
+{
+    public static class ResultSetExtensions
+    {
+        public static IEnumerable<T> ToObjects<T>(this List<Query.Result> results, string target)
+        {
+            List<T> objects = default(List<T>);
+
+            if (results?.Select(x => x?.GetDictionary(target)?.ToDictionary()) is IEnumerable<Dictionary<string, object>> dictionaries)
+            {
+                var settings = new JsonSerializerSettings
+                {
+                    ContractResolver = new ExcludeStreamPropertiesResolver()
+                };
+
+                settings.Converters?.Add(new BlobToBytesJsonConverter());
+
+                objects = new List<T>();
+
+                // TODO: Replace with serializing and parsing the List<Dictionary> directly
+                foreach (var dictionary in dictionaries)
+                {
+                    var json = JsonConvert.SerializeObject(dictionary, settings);
+
+                    if (!string.IsNullOrEmpty(json))
+                    {
+                        var jObj = JObject.Parse(json);
+
+                        if (jObj != null)
+                        {
+                            objects.Add(jObj.ToObject<T>());
+                        }
+                    }
+                }
+            }
+
+            return objects;
+        }
+    }
+}
